a36691bcbb138d68d983f16399cb3de03485495d
mosrourtafadar <mosrourtafadar@gmail.com>
2021-10-08 07:50:05 -0500

    final coomit

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -3,11 +3,16 @@ package com.codepath.apps.restclienttemplate.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Tweet {
+
+    public Tweet(){
+    }
 
     public String body;
     public  String createdAt;
